5d5a504817c384ed5e9d7b4fc6f9c8068041bc4a
José Garcia <jose.ragarcia82@gmail.com>
2022-07-20 19:58:55 -0300

    Finalizado módulo de produtos

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata'
 import express, { Request, Response, NextFunction } from 'express'
+import 'express-async-errors'
 import cors from 'cors'
+import { errors } from 'celebrate'
 import routes from './routes'
 import AppError from '../errors/AppError'
 import '@shared/typeorm';
@@ -10,6 +12,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+app.use(errors())
 
 // Middleware de tratamento de erros
 app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
