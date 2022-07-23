430c3e04f8a6c2b6bcd1b1d8c5d4e91d33b46ace
José Garcia <jose.ragarcia82@gmail.com>
2022-07-23 09:54:12 -0300

    feat: Criado service UpdateUserService

---
+++ src/modules/users/services/UpdateUserService.ts
@@ -0,0 +1,40 @@
+import AppError from "@shared/errors/AppError";
+import { getCustomRepository } from "typeorm";
+import UsersRepository from "../typeorm/entities/repositories/UsersRepository";
+import User from "../typeorm/entities/User";
+
+interface IRequest {
+    id: string
+    name: string
+    email: string
+    password: string
+    avatar: string
+}
+
+class UpdateUserService {
+    public async execute({ id, name, email, password, avatar }: IRequest): Promise<User> {
+        const usersRepository = getCustomRepository(UsersRepository)
+
+        const user = await usersRepository.findOne(id)
+
+        if (!user) {
+            throw new AppError('User not found')
+        }
+
+        const userExists = await usersRepository.findByEmail(email)
+
+        if (userExists && email !== user.email) {
+            throw new AppError('There is already an user with this email')
+        }
+
+        user.name = name
+        user.password = password
+        user.avatar = avatar
+
+        await usersRepository.save(user)
+
+        return user
+    }
+}
+
+export default UpdateUserService
