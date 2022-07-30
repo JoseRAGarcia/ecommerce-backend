7641672dfb2d3b3e8dd4a99ee62fae1c0a9b14b1
José Garcia <jose.ragarcia82@gmail.com>
2022-07-30 10:25:08 -0300

    feat: Criado overwrite @types/express e retornado id do usuário em middleware isAuthenticated

--- src/shared/http/middlewares/isAuthenticated.ts
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -3,21 +3,32 @@ import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 import authConfig from '@config/auth'
 
+interface TokenPayload {
+    iat: number
+    exp: number
+    sub: string
+}
+
 export default function isAuthenticated(request: Request, response: Response, next: NextFunction,): void {
     const authHeader = request.headers.authorization
 
-    if(!authHeader) {
+    if (!authHeader) {
         throw new AppError('JWT token is missing')
     }
 
     const [, token] = authHeader.split(' ')
 
     try {
-        const decodeToken = verify(token, authConfig.jwt.secret)
+        const decodedToken = verify(token, authConfig.jwt.secret)
+        const { sub } = decodedToken as TokenPayload
 
+        request.user = {
+            id: sub
+        }
+        
         return next()
     } catch {
         throw new AppError('Invalid JWT token')
     }
-    
+
 }
