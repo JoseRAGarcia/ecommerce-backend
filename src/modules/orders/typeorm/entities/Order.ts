752481de2cab24f7ff0abdd92b5fdbe9c3de5bc2
José <jose.ragarcia82@gmail.com>
2023-01-26 13:26:25 -0300

    feat: Criada entidade OrdersProducts

--- src/modules/orders/typeorm/entities/Order.ts
+++ src/modules/orders/typeorm/entities/Order.ts
@@ -1,5 +1,6 @@
 import Customer from "@modules/customers/typeorm/entities/Customer";
-import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import OrdersProducts from "./OrdersProducts";
 
 @Entity('orders')
 class Order {
@@ -9,6 +10,11 @@ class Order {
     @ManyToOne(() => Customer)
     @JoinColumn({ name: 'customer_id' })
     customer: Customer;
+
+    @OneToMany(() => OrdersProducts, orders_products => orders_products.order, {
+        cascade: true,
+    })
+    orders_products: OrdersProducts[]
 
     @CreateDateColumn()
     created_at: Date;
