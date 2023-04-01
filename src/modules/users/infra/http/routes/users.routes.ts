dabb02621e3d1f1415ffe01c56a2e55199db56cf
José <jose.ragarcia82@gmail.com>
2023-04-01 12:24:16 -0300

    feat: reestruturado arquivos de infra e criado models de customers

---
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -0,0 +1,66 @@
+import { Router } from 'express'
+import UsersController from '../controllers/UsersController'
+import { celebrate, Joi, Segments } from 'celebrate'
+import isAuthenticated from '../../../../../shared/infra/http/middlewares/isAuthenticated'
+import multer from 'multer'
+import uploadConfig from '@config/upload'
+import UserAvatarController from '../controllers/UserAvatarController'
+
+const usersRouter = Router()
+const usersController = new UsersController()
+const userAvatarController = new UserAvatarController()
+
+const upload = multer(uploadConfig.multer)
+
+usersRouter.get('/', isAuthenticated, usersController.index)
+usersRouter.get(
+    '/:id',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.string().uuid().required(),
+        },
+    }),
+    usersController.show
+)
+usersRouter.post(
+    '/',
+    celebrate({
+        [Segments.BODY]: {
+            name: Joi.string().required(),
+            email: Joi.string().email().required(),
+            password: Joi.string().required(),
+        }
+    }),
+    usersController.create,
+)
+usersRouter.put(
+    '/:id',
+    celebrate({
+        [Segments.BODY]: {
+            name: Joi.string().required(),
+            email: Joi.string().email().required(),
+            password: Joi.string().required(),
+        },
+        [Segments.PARAMS]: {
+            id: Joi.string().uuid().required(),
+        },
+    }),
+    usersController.update
+)
+usersRouter.patch(
+    '/avatar',
+    isAuthenticated,
+    upload.single('avatar'),
+    userAvatarController.update,
+)
+usersRouter.delete(
+    '/:id',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.string().uuid().required(),
+        },
+    }),
+    usersController.delete
+)
+
+export default usersRouter
